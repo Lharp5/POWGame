c8ce1db0636da58a22501ddd57c0f6bb3fba3a5d
ColinZeidler <c.zeidler.wc@gmail.com>
2013-04-04 01:43:25 -0400

    adding to the view

--- src/LauncherView.java
+++ src/LauncherView.java
@@ -1,7 +1,12 @@
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
 public class LauncherView extends JPanel{
+	JPanel playerOnePanel, playerTwoPanel;
+	JButton[] p1Character, p2Charater;
+	JButton startGame;
+	
 	
 	public LauncherView() {
 		setSize(1280, 720);
