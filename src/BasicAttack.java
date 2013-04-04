221ebbc4f2639f0eb9a7629dbd848db79e887210
ColinZeidler <c.zeidler.wc@gmail.com>
2013-04-04 01:05:18 -0400

    adding fields to attacks

--- src/BasicAttack.java
+++ src/BasicAttack.java
@@ -1,9 +1,11 @@
 
 public class BasicAttack {
-
-	/**
-	 * @param args
-	 */
+	private String type;
+	private int damage;
+	private int range;
+	private int actionsPS;
+	private int actionDuration;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
