819f8afaa53b80a88920311d01d1e93ee8dbe2ad
ColinZeidler <c.zeidler.wc@gmail.com>
2013-04-05 01:09:10 -0400

    changing how the button lists are created
    allowing for additional fighters to be added

--- src/LauncherView.java
+++ src/LauncherView.java
@@ -71,8 +71,8 @@ public class LauncherView extends JPanel {
 		playerOnePanel.setLayout(player1Layout);
 
 		// buttons
-		p1Character = new JButton[4];
-		for (int i = 0; i < 4; i++) {
+		p1Character = new JButton[model.getFighters.length];
+		for (int i = 0; i < p1Character.length; i++) {
 			p1Character[i] = new JButton("Test" + i);
 			p1Const.gridx = i;
 			p1Const.gridy = 0;
@@ -93,8 +93,8 @@ public class LauncherView extends JPanel {
 		playerTwoPanel.setLayout(player2Layout);
 
 		// buttons
-		p2Character = new JButton[4];
-		for (int i = 0; i < 4; i++) {
+		p2Character = new JButton[model.getFighters.length];
+		for (int i = 0; i < p2Character.length; i++) {
 			p2Character[i] = new JButton("Test" + i);
 			p2Const.gridx = i;
 			p2Const.gridy = 0;
