04da39a1cb2e3bbfa16d315d29cfedba8484f25b
Luke Harper <lukeharper@live.ca>
2013-04-05 01:04:25 -0400

    Battle Class added, expanded Fighter etc.
    I added battle class which will handle all events
    Expanded fighter to make it more functional.
    Added future file reader and loader for FighterPool

--- src/Fighter.java
+++ src/Fighter.java
@@ -4,11 +4,19 @@ public class Fighter {
 		//attributes every fighter has.
 	
 		private String name;
+		
 		private BasicAttack [] attacks;
+		
 		private Ability [] abilities;
 		private Block block;
 		private int life;
 		private int speed;
+		private boolean air;
+		
+		//getter and setting methods for air function.
+		public boolean inAir(){		return air;		}
+		public void startJump(){	air = true;		}
+		public void endJump(){		air = false;	}
 		
 		
 		public Fighter(String n, BasicAttack [] a, Ability [] ab, Block b, int s){
@@ -19,6 +27,12 @@ public class Fighter {
 			
 			life=100;	
 			speed =s;
+			
+			//inAir is to tell if a player is jumping in air, to add jumping abilities/fights.
+			air=false;
+		}
+		
+		public void attack(int i){
 		}
 
 }
