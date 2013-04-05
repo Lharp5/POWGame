4e2de5a1a774f35719a7730f4a406f84a2e6549d
Luke Harper <lukeharper@live.ca>
2013-04-05 01:43:29 -0400

    Updates
    EVERYWHERE :D

--- src/Battle.java
+++ src/Battle.java
@@ -1,4 +1,25 @@
 
 public class Battle {
-
+	
+	private Fighter firstPlayer, secondPlayer;
+	
+	public Battle(Fighter a, Fighter b){
+		firstPlayer = a;
+		firstPlayer = b;
+	}
+	
+	//do we need these?
+	public void startGame(){
+	}
+	
+	public void endGame(){
+	}
+	
+	public void firstPlayerAttack(){
+		int damage;
+		if(firstPlayer.inAir()){
+			if((firstPlayer.getX()+firstPlayer.getAttack(0).getRange() >= secondPlayer.getX())&&(true)) //true needs to be changed to height.
+			damage=firstPlayer.getAttack(0).getDamage();
+		}
+	}
 }
