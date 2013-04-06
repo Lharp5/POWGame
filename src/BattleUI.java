6454694e17cdb86600f3272ba8993a6384cf40ff
ColinZeidler <c.zeidler.wc@gmail.com>
2013-04-05 22:11:15 -0400

    Double Buffered
    I has double buffering!

--- src/BattleUI.java
+++ src/BattleUI.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 
 import javax.swing.JPanel;
 
@@ -9,24 +10,45 @@ import javax.swing.JPanel;
 public class BattleUI extends JPanel{
 	private Battle battle;
 	private Fighter player1, player2;
+	private Graphics2D g, dbg;
+	private Image dbImage;
+	double o1x, o1y, o2x, o2y;
 	
 	public BattleUI(Fighter p1, Fighter p2, Battle b) {
 		battle = b;
 		player1 = p1;
 		player2 = p2;
 		setPreferredSize(new Dimension(1280, 720));
+		setDoubleBuffered(true);
 		setVisible(true);
+		
+		o1x = o1y = o2x = o2y = 0;
 	}
 	
-	public void draw() {
-		Graphics g = this.getGraphics();
-		g.setColor(Color.DARK_GRAY);
-		g.fillRect(0, 0, 1290, 730);
-		
+	public void dbDraw() {
+		g = (Graphics2D) this.getGraphics();
+		dbImage = createImage(1290, 730);
+		dbg = (Graphics2D) dbImage.getGraphics();
+		draw(dbg);
+		g.drawImage(dbImage, 0, 0, this);
 	}
+	
+	private void draw(Graphics2D g2) {
+		update();
+		g2.setColor(Color.DARK_GRAY);
+		g2.fillRect(0, 0, 1290, 730);
+		g2.setColor(Color.RED);
+		g2.fillOval((int)o1x, (int)o1y, 50, 50);
+		g2.setColor(Color.BLUE);
+		g2.fillOval((int)o2x, (int)o2y, 60, 20);
+	}
+	
 	
 	public void update() {
-		
+		o1x += 2;
+		o1y += 0.5;
+		o2x +=0.5;
+		o2y += 1;
 	}
 
 }
