b69296c4a551f1d9b92136cea90221f4493584f9
ColinZeidler <c.zeidler.wc@gmail.com>
2013-04-11 23:58:16 -0400

    basic stats added

--- src/LauncherView.java
+++ src/LauncherView.java
@@ -241,6 +241,9 @@ public class LauncherView extends JPanel {
 				p1Character[i].setBackground(Color.RED);
 				p2Character[i].setEnabled(false);
 				p2Character[i].setBackground(Color.RED);
+				p1Stats.setText("Height: " + model[i].getHeight() + " \n" 
+								+ "Speed: "+ model[i].getSpeed() + "\n" 
+								+ "Life: " + model[i].getLife());
 				p1Desc.setText(model[i].getDescription());
 			} else if (selection[i] == 2) {
 				p2ReadyE = true;
@@ -248,6 +251,9 @@ public class LauncherView extends JPanel {
 				p2Character[i].setBackground(Color.BLUE);
 				p1Character[i].setEnabled(false);
 				p1Character[i].setBackground(Color.BLUE);
+				p2Stats.setText("Height: " + model[i].getHeight() + " \n" 
+						+ "Speed: "+ model[i].getSpeed() + "\n" 
+						+ "Life: " + model[i].getLife());
 				p2Desc.setText(model[i].getDescription());
 			} else {
 				p1Character[i].setEnabled(true);
